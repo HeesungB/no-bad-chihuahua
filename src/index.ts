c1bc102e4e7dfb46b8478cdf4d3e4bd87083c8a0
HeesungB <32606183+HeesungB@users.noreply.github.com>
2021-12-22 12:35:46 +0900

    add: express function
    add: listen port 3000
    add: route get '/'

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { mnemonicToSeedSync } from 'bip39';
 import { fromSeed } from 'bip32';
-import schedule from 'node-schedule';
 import { createRawTx, getAccount, PATH, sendTx, signTx } from './services/cosmos';
 import { MNEMONIC } from './config';
 import { getReward } from './services/api';
 import { BN } from 'bn.js';
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
 
 const seed = mnemonicToSeedSync(MNEMONIC);
 const node = fromSeed(seed);
@@ -12,7 +14,7 @@ const node = fromSeed(seed);
 const child = node.derivePath(`m/44'/${PATH}'/0'/0/0`);
 const account = getAccount(child, 'chihuahua');
 
-const job = schedule.scheduleJob('*/1 * * * *', async () => {
+app.get('/', async (req: Request, res: Response, next: NextFunction) => {
   const rewardResponse = await getReward(account.address);
   const validatorAddress = rewardResponse.result.rewards[0].validator_address;
   const amount = new BN(rewardResponse.result.rewards[0].reward[0].amount.split('.')[0], 10);
@@ -21,4 +23,14 @@ const job = schedule.scheduleJob('*/1 * * * *', async () => {
   const rawTx = await createRawTx(account.address, validatorAddress, amount.sub(gasPrice).toString());
   const signedTx = await signTx(child, 'chihuahua', rawTx);
   const result = await sendTx(signedTx);
+
+  res.send(result);
+});
+
+app.listen('3000', () => {
+  console.log(`
+    ################################################
+            Server listening on port: 3000
+    ################################################
+  `);
 });
