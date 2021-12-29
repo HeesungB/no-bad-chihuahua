ed4791e2709992767bbeabef382b87d5141fcb71
HeesungB <32606183+HeesungB@users.noreply.github.com>
2021-12-30 01:15:55 +0900

    fix: add mnemonic type

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,7 @@
+export const MNEMONIC = '';
 export const PRIVATE_KEY = '';
+
+export const COIN_TYPE = 118; // https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+
+export const CHIHUAHUA_RPC_URL = 'https://rpc.chihuahua.wtf';
+export const GAS_PRICE = 210000;
--- src/services/cosmos.ts
+++ src/services/cosmos.ts
@@ -1,32 +1,35 @@
 import { encodeSecp256k1Pubkey, pubkeyToAddress } from '@cosmjs/amino';
 import { AccountData, DirectSecp256k1Wallet, encodePubkey, makeAuthInfoBytes, makeSignDoc } from '@cosmjs/proto-signing';
 import { StargateClient } from '@cosmjs/stargate';
-import { BIP32Interface } from 'bip32';
 import { Account, RawTx, SignedTx } from '../types/types';
 import { TxRaw } from 'cosmjs-types/cosmos/tx/v1beta1/tx';
 import { registry } from '../types/defaultRegistryTypes';
+import { GAS_PRICE } from '../config';
 
-export const PATH = 118;
 declare type CHAIN = 'osmo' | 'chihuahua';
 
-export const getAccount = async (privateKey: string, prefix: CHAIN): Promise<Account> => {
-  var privateKeyBuffer = Buffer.from(privateKey, 'hex');
-  const wallet = await DirectSecp256k1Wallet.fromKey(new Uint8Array(privateKeyBuffer), prefix);
+export const convertHexStringToBuffer = (hexString: string) => Buffer.from(hexString, 'hex');
+
+export const getClient = async (rpcUrl: string): Promise<StargateClient> => {
+  const client = await StargateClient.connect(rpcUrl);
+
+  return client;
+};
+
+export const getAccount = async (privateKey: Buffer, prefix: CHAIN): Promise<Account> => {
+  const wallet = await DirectSecp256k1Wallet.fromKey(new Uint8Array(privateKey), prefix);
   const accounts = await wallet.getAccounts();
   const accountData: AccountData = accounts[0];
 
   return { address: accountData.address, publicKey: accountData.pubkey.toString() };
 };
 
-const getClient = async (): Promise<StargateClient> => {
-  const client = await StargateClient.connect('https://rpc.chihuahua.wtf');
-
-  return client;
-};
-
-export const createRawTx = async (delegatorAddress: string, validatorAddress: string, amount: string): Promise<RawTx> => {
-  const client = await getClient();
-
+export const createClaimAndDelegateRawTx = async (
+  client: StargateClient,
+  delegatorAddress: string,
+  validatorAddress: string,
+  amount: string
+): Promise<RawTx> => {
   const sequence = await client.getSequence(delegatorAddress);
   const chainId = await client.getChainId();
 
@@ -43,7 +46,7 @@ export const createRawTx = async (delegatorAddress: string, validatorAddress: st
           amount: '7000',
         },
       ],
-      gas: '200000',
+      gas: GAS_PRICE,
     },
     memo: '',
     msgs: [
@@ -54,6 +57,90 @@ export const createRawTx = async (delegatorAddress: string, validatorAddress: st
           validatorAddress,
         },
       },
+      {
+        typeUrl: '/cosmos.staking.v1beta1.MsgDelegate',
+        value: {
+          delegatorAddress,
+          validatorAddress,
+          amount: {
+            amount,
+            denom: 'uhuahua',
+          },
+        },
+      },
+    ],
+    sequence: `${sequence.sequence}`,
+  };
+
+  return rawTx;
+};
+
+export const createClaimRawTx = async (
+  client: StargateClient,
+  delegatorAddress: string,
+  validatorAddress: string,
+  amount: string
+): Promise<RawTx> => {
+  const sequence = await client.getSequence(delegatorAddress);
+  const chainId = await client.getChainId();
+
+  const rawTx: RawTx = {
+    signerData: {
+      accountNumber: `${sequence.accountNumber}`,
+      sequence: sequence.sequence,
+      chainId,
+    },
+    fee: {
+      amount: [
+        {
+          denom: 'uhuahua',
+          amount: '7000',
+        },
+      ],
+      gas: GAS_PRICE,
+    },
+    memo: '',
+    msgs: [
+      {
+        typeUrl: '/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward',
+        value: {
+          delegatorAddress,
+          validatorAddress,
+        },
+      },
+    ],
+    sequence: `${sequence.sequence}`,
+  };
+
+  return rawTx;
+};
+
+export const createDelegateRawTx = async (
+  client: StargateClient,
+  delegatorAddress: string,
+  validatorAddress: string,
+  amount: string
+): Promise<RawTx> => {
+  const sequence = await client.getSequence(delegatorAddress);
+  const chainId = await client.getChainId();
+
+  const rawTx: RawTx = {
+    signerData: {
+      accountNumber: `${sequence.accountNumber}`,
+      sequence: sequence.sequence,
+      chainId,
+    },
+    fee: {
+      amount: [
+        {
+          denom: 'uhuahua',
+          amount: '7000',
+        },
+      ],
+      gas: GAS_PRICE,
+    },
+    memo: '',
+    msgs: [
       {
         typeUrl: '/cosmos.staking.v1beta1.MsgDelegate',
         value: {
@@ -114,9 +201,7 @@ export const signTx = async (privateKeyBuffer: Buffer, prefix: CHAIN, rawTx: Raw
   return { rawTx, signedTx: { txRaw } };
 };
 
-export const sendTx = async (signedTx: SignedTx): Promise<string> => {
-  const client = await getClient();
-
+export const sendTx = async (client: StargateClient, signedTx: SignedTx): Promise<string> => {
   const txRawCall = signedTx.signedTx.txRaw;
   const txBytes = TxRaw.encode(txRawCall).finish();
   const response = await client.broadcastTx(txBytes);
